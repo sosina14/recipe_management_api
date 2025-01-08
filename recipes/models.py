32982732e211e7a8d7e441b7a22ac06cee350bbe
Sosina <sosina14@gmail.com>
2025-01-08 23:16:50 +0300

    Defined Recipe model, applied migrations, and registered model in Admin Panel

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ('Dessert', 'Dessert'),
+        ('Main Course', 'Main Course'),
+        ('Appetizer', 'Appetizer'),
+        ('Breakfast', 'Breakfast'),
+        ('Vegetarian', 'Vegetarian'),
+    ]
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()  # Comma-separated list of ingredients
+    instructions = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    preparation_time = models.IntegerField()  # in minutes
+    cooking_time = models.IntegerField()  # in minutes
+    servings = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+
+    def __str__(self):
+        return self.title
